ae79ffb96be3cb4f6c7eb76e5346df8fc924eb9d
jacube1 <jakubfedorczyk@gmail.com>
2019-12-02 16:42:24 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Wydatek, KATEGORIE } from './wydatek';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
+@Component
+(
+  {
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
+  }
+)
+export class AppComponent  
+{
   name = 'Angular';
+}
+export class AppComponent  
+{
+    wydatki: Wydatek[];
 }
