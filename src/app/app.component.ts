64857189803a1c4514a9c6dba9b7105d52c2054c
jacube1 <jakubfedorczyk@gmail.com>
2019-12-15 20:44:52 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ ngOnInit()
       new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
       new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
       new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
-      new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12))
+      new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),1.1,1.2),
+      new Wydatek('Tankowanie', 325.20, 'Do pełna wczoraj', new Date(2017, 8, 12),'',4.5)
     ];
   }
 }
