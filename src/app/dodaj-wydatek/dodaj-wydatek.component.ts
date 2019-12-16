2ffe8cd6944f388a9c18f56d38d6cd4990ff98c7
jacube1 <jakubfedorczyk@gmail.com>
2019-12-16 17:10:59 +0100

    1 file modified

--- src/app/dodaj-wydatek/dodaj-wydatek.component.ts
+++ src/app/dodaj-wydatek/dodaj-wydatek.component.ts
@@ -11,20 +11,19 @@ export class DodajWydatekComponent implements OnInit
 {
   nowyWydatek: Wydatek;
   kategorie: string[];
-
 constructor(private wydatkiService: WydatkiService, private router: Router) 
-  { 
+{
 
-  }
+}
   ngOnInit() 
   {
     this.nowyWydatek = new Wydatek('Tankowanie', null, null, null);
     this.kategorie = this.wydatkiService.getKategorie();
-    this.router.navigate(['/wydatki']);
   }
   onSubmit() 
   {
     this.wydatkiService.dodajWydatek(this.nowyWydatek);
     this.nowyWydatek = new Wydatek('Tankowanie', null, null, null);
+    this.router.navigate(['/wydatki']);
   }
 }
