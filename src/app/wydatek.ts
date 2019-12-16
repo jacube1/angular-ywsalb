c7fc426f1a9f805f32fcbc8a5cd4dd763f74c3e8
jacube1 <jakubfedorczyk@gmail.com>
2019-12-16 17:15:48 +0100

    2 files modified

--- src/app/wydatek.ts
+++ src/app/wydatek.ts
@@ -1,14 +1,14 @@
-export class Wydatek 
-{
-    constructor
-      (
+export class Wydatek {
+  constructor
+  (
+        public id: number,
         public kategoria: string,
         public kwota: number,
         public opis: string,
         public data: Date,
         public ileLitrow: number,
         public ileKm: number,
-      ) 
+  ) 
     {
 
     }
--- src/app/wydatki.service.ts
+++ src/app/wydatki.service.ts
@@ -8,12 +8,12 @@ export class WydatkiService
   { 
     this.wydatki = 
     [
-      new Wydatek('Tankowanie', 252.43, '', new Date(2018, 4, 30),1.1),
-      new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
-      new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16),'',50),
-      new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
-      new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),1.1,1.2),
-      new Wydatek('Tankowanie', 325.20, 'Do pełna wczoraj', new Date(2017, 8, 12),'',4.5)
+      new Wydatek(1,'Tankowanie', 252.43, '', new Date(2018, 4, 30),1.1),
+      new Wydatek(2,'Myjnia', 75, '', new Date(2018, 4, 2)),
+      new Wydatek(3,'Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16),'',50),
+      new Wydatek(4,'Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
+      new Wydatek(5,'Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),1.1,1.2),
+      new Wydatek(6,'Tankowanie', 325.20, 'Do pełna wczoraj', new Date(2017, 8, 12),'',4.5)
     ];
   }
   getWydatki() 
