e6f139aab4a6db104d2d7d3505bb1aa118a35a6a
jacube1 <jakubfedorczyk@gmail.com>
2019-12-16 16:13:12 +0100

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListaWydatkowComponent } from './lista-wydatkow/lista-wydatkow.component';
 import { WydatkiService } from './wydatki.service';
+import { DodajWydatekComponent } from './dodaj-wydatek/dodaj-wydatek.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ListaWydatkowComponent ],
+  declarations: [ AppComponent, ListaWydatkowComponent, DodajWydatekComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [WydatkiService]
 })
---
+++ src/app/dodaj-wydatek/dodaj-wydatek.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dodaj-wydatek',
+  templateUrl: './dodaj-wydatek.component.html',
+  styleUrls: ['./dodaj-wydatek.component.css']
+})
+export class DodajWydatekComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
