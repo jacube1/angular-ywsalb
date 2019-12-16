0605bb8b20398275780ea89829f83a513eccd5cf
jacube1 <jakubfedorczyk@gmail.com>
2019-12-16 16:22:38 +0100

    2 files modified

--- src/app/dodaj-wydatek/dodaj-wydatek.component.ts
+++ src/app/dodaj-wydatek/dodaj-wydatek.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Wydatek } from '../wydatek';
 @Component({
   selector: 'app-dodaj-wydatek',
   templateUrl: './dodaj-wydatek.component.html',
   styleUrls: ['./dodaj-wydatek.component.css']
 })
-export class DodajWydatekComponent implements OnInit {
+export class DodajWydatekComponent implements OnInit 
+{
+  constructor() 
+  { 
 
-  constructor() { }
-
-  ngOnInit() {
   }
+  ngOnInit() 
+  {
 
+  }
 }
--- src/app/wydatki.service.ts
+++ src/app/wydatki.service.ts
@@ -8,9 +8,9 @@ export class WydatkiService
   { 
     this.wydatki = 
     [
-      new Wydatek('Tankowanie', 252.43, '', new Date(2018, 4, 30),1.1,),
+      new Wydatek('Tankowanie', 252.43, '', new Date(2018, 4, 30),1.1),
       new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
-      new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
+      new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16),'',50),
       new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
       new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),1.1,1.2),
       new Wydatek('Tankowanie', 325.20, 'Do pełna wczoraj', new Date(2017, 8, 12),'',4.5)
