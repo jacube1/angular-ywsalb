4bbf0a047d83ae413c4afe99e53aa333eb813f3e
jacube1 <jakubfedorczyk@gmail.com>
2019-12-16 16:57:22 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { ListaWydatkowComponent } from './lista-wydatkow/lista-wydatkow.component';
 import { WydatkiService } from './wydatki.service';
 import { DodajWydatekComponent } from './dodaj-wydatek/dodaj-wydatek.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'wydatki', component: ListaWydatkowComponent },
+  { path: 'dodaj', component: DodajWydatekComponent },
+  { path: '',
+    redirectTo: '/wydatki',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   declarations: [ AppComponent, ListaWydatkowComponent, DodajWydatekComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [WydatkiService]
